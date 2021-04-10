96bd9e9eaf6e1590eecb0d1d6713339cc57bf603
moaad99 <moadlzrak@gmail.com>
2021-04-10 21:31:07 +0100

    fi

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -4,7 +4,7 @@ import {loginUrl} from "../tools/spotify"
 function Login() {
     return (
         <div className="login">
-            <img src="https://www.djmagitalia.com/wp-content/uploads/2016/03/spotify-logo-horizontal-black.jpg" alt=""/>
+            <img style={{width:"600px",height:"300px"}} src="https://www.djmagitalia.com/wp-content/uploads/2016/03/spotify-logo-horizontal-black.jpg" alt=""/>
        
             <a href={loginUrl}>Login with Spotify</a>
        
